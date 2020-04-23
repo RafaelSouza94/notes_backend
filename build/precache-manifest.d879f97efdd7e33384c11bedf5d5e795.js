8e3e1b4ef0702e6f838b337f78d91f22321951f8
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-23 11:40:40 -0700

    add frontend

---
+++ build/precache-manifest.d879f97efdd7e33384c11bedf5d5e795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9ad92530e16ae1670dd846e6d3743a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73686cde0b1616404884",
+    "url": "/static/css/main.6edcfdc4.chunk.css"
+  },
+  {
+    "revision": "116510e748825c439f11",
+    "url": "/static/js/2.bd802056.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd802056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73686cde0b1616404884",
+    "url": "/static/js/main.5b033b1c.chunk.js"
+  },
+  {
+    "revision": "d540d796d2993bde2c1a",
+    "url": "/static/js/runtime-main.17f5897d.js"
+  }
+]);
