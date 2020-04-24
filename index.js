aeed9fc49964d9c97b03020a77a672a6fd47a83d
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-24 13:01:05 -0700

    finished part3c

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
+
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>')
 })
@@ -50,7 +51,7 @@ app.get('/api/notes', (req, res) => {
 	})
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
 	Note.findById(request.params.id).then(note => {
 		if (note){
 			response.json(note.toJSON())
@@ -58,18 +59,15 @@ app.get('/api/notes/:id', (request, response) => {
 			response.status(404).end()
 		}
 	})
-	.catch(error => {
-		console.log(error);
-		response.status(400).send({ Error: 'Malformed id' })
-	})		
+	.catch(error => next(error))		
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id)
-	notes = notes.filter(note => note.id !== id)
-
-	response.status(204).end()
-
+	Note.findByIdAndRemove(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -92,11 +90,38 @@ app.post('/api/notes', (request, response) => {
 	})
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+	const body = request.body
+
+	const note = {
+		content: body.content, 
+		important: body.important,
+	}
+
+	Note.findByIdAndUpdate(request.params.id, note, {new: true})
+		.then(updatedNote => {
+			response.json(updatedNote.toJSON())
+		})
+		.catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ Error: 'Unknown endpoint'})
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ Error: 'Malformed id' })
+	}
+
+	next(error)
+}
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
